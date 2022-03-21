e185b7fb7411a11f19610aac48b1aa4f91762a04
sean <midruss@yahoo.com>
2022-03-21 17:43:04 -0400

    complete lucasNumberMemo

--- memoization_project/lib/problems.js
+++ memoization_project/lib/problems.js
@@ -17,6 +17,12 @@
 // lucasNumberMemo(42)  // => 599074578
 function lucasNumberMemo(n, memo = {}) {
 
+    if (n in memo) return memo[n]; //fetching value of n if it's in the memo already
+    if (n === 0) return 2;
+    if (n === 1) return 1;
+
+    memo[n] = lucasNumberMemo(n-1, memo) + lucasNumberMemo(n-2, memo); // storing value of n in the memo for future reference
+    return memo[n];
 }
 
 
